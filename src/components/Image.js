ad534a1676681b882fb2aea922687c29050988c5
Oliver <oliver.nilsson94@gmail.com>
2019-12-07 21:05:57 +0100

    hover effect on images

--- src/components/Image.js
+++ src/components/Image.js
@@ -8,6 +8,10 @@ const Image = props => {
     height: 250px;
     margin 30px;
     display: inline-block;
+    transition: 200ms ease-in-out;
+    &:hover {
+      transform: scale(1.05);
+      }
   `
   return <Image></Image>
 }
